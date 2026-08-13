65db527c620ecfd9095acc70de2e6c87a4114d97
cozywitchcraft <activejoey60@protonmail.com>
2026-08-13 19:27:39 -0400

    remove typing while reading message history

--- __main__.py
+++ __main__.py
@@ -78,9 +78,7 @@ class Channel():
 async def add_channel(channel):
     # TODO: Channel setting commands
     channels[channel.id] = Channel(channel)
-
-    async with channel.typing():
-        await channels[channel.id].read_message_history()
+    await channels[channel.id].read_message_history()
 
 def generate_message(channel):
     return channels[channel.id].generate_message()
